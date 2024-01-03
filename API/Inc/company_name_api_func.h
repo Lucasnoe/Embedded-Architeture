d86917291909fc86cea4a221fbe461dcdd823d1a
Lucas Noé <lucas.nooe@outlook.com>
2024-01-03 14:33:13 -0300

    added HAL Files

---
+++ API/Inc/company_name_api_func.h
@@ -0,0 +1,26 @@
+
+/**
+  ******************************************************************************
+  * @Company    : Name of company
+  * @file       : name_of_file.h
+  * @author     : Name of author
+  * @version	: version of driver, API or application files 
+  * @date       : Data of the file was created
+  * @brief      : What this file does
+  ******************************************************************************
+*/ 
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __COMPANY_NAME_API_FUNC_H
+#define __COMPANY_NAME_API_FUNC_H
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+/* Define --------------------------------------------------------------------*/
+/* Typedef -------------------------------------------------------------------*/
+/* Public objects ------------------------------------------------------------*/
+
+
+#endif /* __COMPANY_NAME_API_FUNC_H */
+
+/*****************************END OF FILE**************************************/
